64ea001d863f94f4e454c074fe127d5abd711763
Kiffin Gish <kiffin.gish@planet.nl>
2017-04-15 20:55:47 +0200

    Push all engine migration files into base application migrations list

--- lib/geolocation/engine.rb
+++ lib/geolocation/engine.rb
@@ -1,5 +1,19 @@
 module Geolocation
   class Engine < ::Rails::Engine
     isolate_namespace Geolocation
+
+    initializer "geolocation", before: :load_config_initializers do |app|
+
+      # Let engine mount itself.
+      Rails.application.routes.append do
+        mount Geolocation::Engine, at: "/geolocation"
+      end
+
+      # Push all engine migration files into base application migrations list
+      config.paths["db/migrate"].expanded.each do |expanded_path|
+        Rails.application.config.paths["db/migrate"] << expanded_path
+      end
+    end
+
   end
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  mount Geolocation::Engine => "/geolocation"
+  # mount Geolocation::Engine => "/geolocation"
 end
