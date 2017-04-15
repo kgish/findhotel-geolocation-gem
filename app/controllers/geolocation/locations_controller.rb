4923f31341536cf73faf7f645fa19a72d6ce89ee
Kiffin Gish <kiffin.gish@planet.nl>
2017-04-15 13:16:24 +0200

    Service import_data method

--- app/controllers/geolocation/locations_controller.rb
+++ app/controllers/geolocation/locations_controller.rb
@@ -1,7 +1,10 @@
+require 'csv'
+
 require_dependency "geolocation/application_controller"
 
 module Geolocation
   class LocationsController < ApplicationController
+    skip_before_action :verify_authenticity_token
     before_action :set_location, only: [:show]
 
     # GET /locations
@@ -11,6 +14,30 @@ module Geolocation
 
     # GET /locations/1
     def show
+    end
+
+    # POST /import_data.json
+    def import_data
+
+      start = Time.now
+
+      data_dump_csv = "#{Rails.root}/../../uploads/data_dump.csv"
+
+      data = CSV.read(data_dump_csv)
+
+      # Statistics
+      now = Time.now
+      elapsed = now - start
+
+      render json: { import_data: {
+          dumpfile: data_dump_csv,
+          records: data.count - 1,
+          stopwatch: {
+              started: start.to_s,
+              finished: now.to_s,
+              elapsed: elapsed.to_s
+          }
+      } }
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Geolocation::Engine.routes.draw do
   resources :locations, only: ['index', 'show']
+  post '/import_data' => 'locations#import_data'
 end
