5a5c9e9255735ad408dd41f31400bccb3bdfec29
Kiffin Gish <kiffin.gish@planet.nl>
2017-04-17 21:06:53 +0200

    allow_blank: true for certain fields

--- app/models/geolocation/location.rb
+++ app/models/geolocation/location.rb
@@ -1,7 +1,7 @@
 module Geolocation
   class LatitudeValidator < ActiveModel::Validator
     def validate(record)
-      unless !record.latitude.nil? && record.latitude > -90 && record.latitude < 90
+      if !record.latitude.blank? && (record.latitude < -90 || record.latitude > 90)
         record.errors[:latitude] << 'must lie between -90 and +90'
       end
     end
@@ -9,7 +9,7 @@ module Geolocation
 
   class LongitudeValidator < ActiveModel::Validator
     def validate(record)
-      unless !record.longitude.nil? && record.longitude > -180 && record.longitude < 180
+      if !record.longitude.blank? && (record.longitude < -180 || record.longitude > 180)
         record.errors[:longitude] << 'must lie between -180 and +180'
       end
     end
@@ -20,8 +20,9 @@ module Geolocation
     validates_with LatitudeValidator
     validates_with LongitudeValidator
 
-    validates :ip_address, :country_code, :country, :city, :latitude, :longitude, :mystery_value, presence: true
-    validates :country_code, format: { :with => /\A[A-Z][A-Z]\z/i, message: 'only allows two letters' }
-    validates :city, uniqueness: {scope: [:country_code, :country]}
+    validates :ip_address, :country, presence: true
+    validates :latitude, :longitude, :mystery_value, presence: true, allow_blank: true
+    validates :country_code, format: { :with => /\A[A-Z][A-Z]\z/i, message: 'only allows two letters' }, allow_blank: true
+    validates :city, uniqueness: {scope: [:country_code, :country]}, allow_blank: true
   end
 end
