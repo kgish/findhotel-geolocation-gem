08a0573b29517d27c5378a7c2691ab7b78b5d531
Kiffin Gish <kiffin.gish@planet.nl>
2017-04-18 20:49:01 +0200

    test controller location

--- test/controllers/geolocation/locations_controller_test.rb
+++ test/controllers/geolocation/locations_controller_test.rb
@@ -18,5 +18,20 @@ module Geolocation
       assert_response :success
     end
 
+    test "should get ip_address" do
+      get ip_address_url @location.ip_address
+      assert_response :success
+    end
+
+    test "should return error on invalid ip_address" do
+      get ip_address_url '123456'
+      assert_response :unprocessable_entity
+    end
+
+    test "should return 404 on ip_address not found" do
+      get ip_address_url '200.166.141.15'
+      assert_response :not_found
+    end
+
   end
 end
